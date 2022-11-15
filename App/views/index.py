ce5bbb04b163b9291c2153bdc36811ab65788b5d
Yanderei <glamorchique@gmail.com>
2022-11-15 17:12:56 +0000

    login and sign up layout and routes

--- App/views/index.py
+++ App/views/index.py
@@ -5,3 +5,11 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 @index_views.route('/', methods=['GET'])
 def index_page():
     return render_template('index.html')
+
+@index_views.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+@index_views.route('/signup', methods=['GET'])
+def signup_page():
+    return render_template('signup.html')
