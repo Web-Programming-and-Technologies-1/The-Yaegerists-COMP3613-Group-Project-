89b5e5fd50fce914863788e3c170639d429238ad
Yanderei <glamorchique@gmail.com>
2022-11-29 23:49:58 +0000

    started distribution controller

--- App/controllers/distribution.py
+++ App/controllers/distribution.py
@@ -1,4 +1,18 @@
-#query all profiles
-#choose 4 random indexes
+from App.models import Distribution, Profile
+from App.database import db
+import random
+from sqlalchemy.exc import IntegrityError
+
+
+def distribute():
+    #query all profiles
+    distributionProfiles= []
+    distributionProfiles = Profile.query.all()
+    totalProfileNum = len(distributionProfiles)
+
+    #choose 4 random indexes
+    rand = random.randint(0, totalProfileNum)
+    
+
 #get those profiles by id
 #return the profiles
