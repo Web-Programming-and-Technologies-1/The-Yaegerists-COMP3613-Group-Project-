c35e4bcc6de983758045cdb72eb3b2388b8dae55
joshuaali8 <alijoshua12@hotmail.com>
2022-12-04 01:55:25 +0000

    doing some work

--- App/forms.py
+++ App/forms.py
@@ -4,16 +4,16 @@ from wtforms.validators import InputRequired, EqualTo, Email
 from flask_wtf.file import FileField
 
 class SignUp(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
+    username = StringField('Enter Username', validators=[InputRequired()])
     #name = StringField('name', validators=[InputRequired()])
-    email = StringField('Email', validators=[Email(), InputRequired()])
-    password = PasswordField('New Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
+    email = StringField('Enter Email', validators=[Email(), InputRequired()])
+    password = PasswordField('Enter New Password', validators=[InputRequired(), EqualTo('confirm', message='Passwords must match')])
     confirm  = PasswordField('Repeat Password')
     submit = SubmitField('Sign Up', render_kw={'class': 'btn waves-effect waves-light white-text'})
 
 class LogIn(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
+    username = StringField('Enter Username', validators=[InputRequired()])
+    password = PasswordField('Enter Password', validators=[InputRequired()])
     submit = SubmitField('Login', render_kw={'class': 'btn waves-effect waves-light white-text'})
 
 class UploadPicture(FlaskForm):
